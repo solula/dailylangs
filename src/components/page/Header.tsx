3b8b605dc278daa433bb8747f4d472d93470200e
solula <86023773+solula@users.noreply.github.com>
2024-01-25 02:08:23 +0300

    12 | Сделана адаптивность у Header

--- src/components/page/Header.tsx
+++ src/components/page/Header.tsx
@@ -1,9 +1,25 @@
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import SelectBox from "../ui/Selectbox/Selectbox";
 
 const Header = () => {
-    const navigate = useNavigate();
+    const [screenSize, setScreenSize] = useState(window.innerWidth);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setScreenSize(window.innerWidth);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    return screenSize < 1000 ? <SmallHeader /> : <BigHeader />;
+};
+
+const BigHeader = () => {
     return (
         <header className="header">
             <img className="header__logo" src="./logo.svg" />
@@ -29,6 +45,26 @@ const Header = () => {
                         src="./user.svg"
                     />
                     <SelectBox options={["RU", "ENG", "ES"]} />
+                </nav>
+            </nav>
+        </header>
+    );
+};
+
+const SmallHeader = () => {
+    return (
+        <header className="header">
+            <img className="header__logo--mobile" src="./logo.svg" />
+            <nav className="header__menu">
+                <nav className="header__menu__icon-list">
+                    <img
+                        className="header__menu__icon-list__user-icon--mobile"
+                        src="./user.svg"
+                    />
+                    <img
+                        className="header__menu__icon-list__menu-icon"
+                        src="./menu-icon.svg"
+                    />
                 </nav>
             </nav>
         </header>
